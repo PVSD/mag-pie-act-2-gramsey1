1efa3321d06a0349fd763950d0e429c61db74332
gramsey1 <graceramsey312@yahoo.com>
2018-11-01 11:09:27 -0400

    tell me more about your code.

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -28,19 +28,26 @@ public class Magpie2
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
-	public String getResponse(String statement)
-	{
+	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
+		if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
+		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0)
+
 		{
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0)
+		{
+			response= "Tell me more about your pets.";
+	}
+		else if (statement.indexOf("Mr. ") >= 0
+				|| statement.indexOf("Mrs. ") >= 0)
+		{
+			response= "He sounds like a good teacher.";
 		}
 		else
 		{
